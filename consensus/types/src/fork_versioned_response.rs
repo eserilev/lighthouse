4762ced5f052fe0bb496966ccaada66656732e42
Eitan Seri-Levi <eserilev@ucsc.edu>
2025-04-21 13:38:17 -0700

    revert

--- consensus/types/src/fork_versioned_response.rs
+++ consensus/types/src/fork_versioned_response.rs
@@ -34,8 +34,6 @@ impl std::fmt::Display for ForkVersionDeserializeError {
     }
 }
 
-pub type ForVersionResponseError = Box<dyn StdError + Send + Sync>;
-
 pub trait ForkVersionDecode: Sized {
     /// SSZ decode with explicit fork variant.
     fn from_ssz_bytes_by_fork(bytes: &[u8], fork_name: ForkName) -> Result<Self, ssz::DecodeError>;
